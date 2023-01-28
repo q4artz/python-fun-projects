dcd95e95cb3952b1e65a2d3c5b257b7e2e15ab78
q4artz <116222352+q4artz@users.noreply.github.com>
2023-01-29 02:07:34 +0800

    Add files via upload

--- Sockets/ReceivingSocket.py
+++ Sockets/ReceivingSocket.py
@@ -2,13 +2,13 @@ import socket
 from constant import *
 
 with WebSocket as s:
-    s.bind((HOST,RECEIVINGPORT))
+    s.bind((HOST,RECEIVINGPORT)) #self
     s.listen()
-    conn,addr = s.accept()
+    conn,addr = s.accept() #take all 
     with conn:
         print(f"Connected by {addr}")
         while True:
-            data = conn.recv(1024)
+            data = conn.recv(1024) #buffer size 1024bytes
             if not data:
                 break
             conn.sendall(data)
