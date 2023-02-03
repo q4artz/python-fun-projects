2dbe63966d49e5153e4b6a85c70f66f486f9cfc4
q4artz <116222352+q4artz@users.noreply.github.com>
2023-02-03 14:01:38 +0800

    Add files via upload

--- Sockets/GETRequest.py
+++ Sockets/GETRequest.py
@@ -1,14 +1,14 @@
 import socket 
 from constant import * 
-      # socket type. AF_INET is the Internet address family for IPv4.  
-      # SOCK_STREAM is the socket type for TCP
+      # AF_INET IPv4.  
+      # SOCK_STREAM socket type for TCP
 WebSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 WebSocket.connect((DOMAIN,CONNECTINGPORT));
-Request = 'GET https://google.com HTTP/2\n\n'.encode();
-WebSocket.send(Request);
+Request = 'GET https://www.youtube.com HTTP/1.1\n\n'.encode(); #convert to UTF8
+WebSocket.sendall(Request);
 
 while True:
-      data = WebSocket.recv(1024)
+      data = WebSocket.recv(1024) ## char type
       if(len(data) < 1):
             break;
       print(data.decode())
